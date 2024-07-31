e14220ba82d01ee2b892583a1d3ce92fb03ccd71
wangyh <1037790353@qq.com>
2024-07-31 17:43:07 +0800

    refactor:  lazy加载工具组件

--- components/instrument/index.tsx
+++ components/instrument/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 
-import { Empty } from "antd";
+import { Empty, Spin } from "antd";
 
 interface InstrumentProps {
   componentName: string;
@@ -8,23 +8,34 @@ interface InstrumentProps {
 
 const Instrument: React.FC<InstrumentProps> = (props) => {
   const { componentName } = props;
-  const renderComponent = () => {
-    try {
-      const Comp = require(`./utils/${componentName}/index.tsx`).default;
-      return <Comp />;
-    } catch (error) {
-      return (
-        <Empty
-          className="h-full flex flex-col justify-center"
-          description="404"
-        />
-      );
-    } finally {
-      console.log("render component", componentName);
-    }
-  };
 
-  return <div className="flex-1 p-4 bg-slate-50 dark:bg-slate-950 overflow-hidden">{renderComponent()}</div>;
+  const Comp = lazy(() =>
+    import(`./utils/${componentName}/index.tsx`).catch((reason) => {
+      return {
+        default: () => (
+          <Empty
+            className="h-full flex flex-col justify-center"
+            description="404"
+          />
+        ),
+      };
+    })
+  );
+
+  return (
+    <div className="flex-1 p-4 bg-slate-50 dark:bg-slate-950 overflow-hidden">
+      <Suspense
+        fallback={
+          <div className="flex flex-col w-full h-full justify-center items-center">
+            <Spin />
+            <div className="text-sm pt-1 text-blue-400">加载中</div>
+          </div>
+        }
+      >
+        <Comp />
+      </Suspense>
+    </div>
+  );
 };
 
 export default Instrument;
--- pages/instrument/index.tsx
+++ pages/instrument/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import NavMenu from "@/components/NavMenu";
 import Instrument from "@/components/instrument";
 import instrumentMenus from "@/components/data/instrumentMenu";
+import Head from "next/head";
 
 const Page: React.FC<any> = () => {
   const [componentName, setComponent] = useState<string>("");
@@ -20,6 +21,19 @@ const Page: React.FC<any> = () => {
 
   return (
     <div className="flex">
+      <Head>
+        <title>前端开发工具与资源导航站</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="og:title" content="前端开发工具与资源导航站" />
+        <meta
+          name="description"
+          content="为开发人员提供的方便的在线工具集合，具有出色的用户体验。 在线工具 是一个免费开源的方便在线工具集合，供开发人员和 IT 工作人员使用"
+        />
+        <meta
+          property="og:description"
+          content="为开发人员提供的方便的在线工具集合，具有出色的用户体验。 在线工具 是一个免费开源的方便在线工具集合，供开发人员和 IT 工作人员使用"
+        />
+      </Head>
       <NavMenu
         items={instrumentMenus}
         onSelect={onSelect}
